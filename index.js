92ca5bca7cd7c787abc460e69f39bd8b37c94a01
Emanuel Beltran <emmanuelbeltranc@gmail.com>
2025-04-07 17:00:01 -0500

    Console.log de .env eliminado

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const qrcode = require("qrcode");
 const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
 
-console.log("Cloudinary Config:", {
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
